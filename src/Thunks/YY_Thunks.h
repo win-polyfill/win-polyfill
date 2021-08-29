ba8024e0604735e6ba10635871868b96a81d889b
Yonggang Luo <luoyonggang@gmail.com>
2021-08-29 17:33:57 +0800

    For running clang-format properly
    为了避免编译器将 YYThr$AAA 节优化掉 -> In order to avoid the compiler optimize section YYThr$AAA out

--- src/Thunks/YY_Thunks.h
+++ src/Thunks/YY_Thunks.h
@@ -384,7 +384,7 @@ static void* __fastcall try_get_function(
         __CHECK_UNIT_TEST_BOOL(_FUNCTION);                                                            \
         __declspec(allocate(".YYThr$AAA")) static void* _CRT_CONCATENATE(pInit_ ,_FUNCTION) =         \
               reinterpret_cast<void*>(&_CRT_CONCATENATE(try_get_, _FUNCTION));                        \
-        /*为了避免编译器将 YYThr$AAA 节优化掉*/                                                       \
+        /* In order to avoid the compiler optimize section YYThr$AAA out */                           \
         __foreinclude(_CRT_CONCATENATE(pInit_ ,_FUNCTION));                                           \
         __declspec(allocate(".YYThu$AAB")) static void* _CRT_CONCATENATE( pFun_ ,_FUNCTION);          \
         return reinterpret_cast<_CRT_CONCATENATE(_FUNCTION, _pft)>(try_get_function(                  \
