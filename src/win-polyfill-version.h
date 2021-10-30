d23ef3d1eb46dae0407fa98cd6b29610d4d2bcd4
Yonggang Luo <luoyonggang@gmail.com>
2021-10-30 07:26:22 +0000

    Fixes windows version

--- src/win-polyfill-version.h
+++ src/win-polyfill-version.h
@@ -1,5 +1,12 @@
 ﻿#pragma once
 
+#undef NTDDI_VERSION
+#undef WINVER
+#undef _WIN32_WINNT
+
+#define WINVER 0x0A00
+#define _WIN32_WINNT 0x0A00
+
 #include <sdkddkver.h>
 
 #ifndef WP_SUPPORT_VERSION
